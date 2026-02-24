6fee16135d93f3b598c098df4943067b1ad01561
Basavaraj Bhavi <basavarajbhavi683@gmail.com>
2026-02-25 00:21:39 +0530

    created a header.jsx file

---
+++ React_basics/src/Header.jsx
@@ -0,0 +1,15 @@
+
+function Header(){
+
+    return(
+        <>
+            <h1>My First Web sit using React</h1>
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">Contact</a>
+                <a href="#">About</a>
+            </nav>
+        </>
+    );
+}
+export default Header
