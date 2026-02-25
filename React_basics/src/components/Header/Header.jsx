04ccb80d071852e1e1d785e45211f61b6679d482
Basavaraj Bhavi <basavarajbhavi683@gmail.com>
2026-02-26 01:12:34 +0530

    import css file into Header.jsx and add some style

--- React_basics/src/components/Header/Header.jsx
+++ React_basics/src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+import styles from "/src/components/Header/Header.module.css"
 
 function Header(){
 
@@ -5,10 +6,10 @@ function Header(){
         <>
             <h1>My First Web sit using React</h1>
             <nav>
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Contact</a></li>
-                    <li><a href="#">About</a></li>
+                <ul className={styles.menu}>
+                    <li><a className={styles.menuItem} href="#">Home</a></li>
+                    <li><a className={styles.menuItem} href="#">Contact</a></li>
+                    <li><a className={styles.menuItem} href="#">About</a></li>
                 </ul>
             </nav>
         </>
