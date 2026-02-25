8968f5a33bde9d2e73699a8cef6460a015254cb2
Basavaraj Bhavi <basavarajbhavi683@gmail.com>
2026-02-26 01:09:58 +0530

    imported full path of Header and Footer

--- React_basics/src/App.jsx
+++ React_basics/src/App.jsx
@@ -1,12 +1,13 @@
-import Header from "./Header"
-import Footer from "./Footer"
+import Header from "/src/components/Header/Header.jsx"
+import Footer from "/src/components/Footer/Footer.jsx"
 function App() {
   
 
   return (
     <>
-      <Footer />
       <Header/>
+      <Footer />
+      
       
     </>
   )
