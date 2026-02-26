2f037b95dc5312b1b57934c43f831302c43e17cd
Basavaraj Bhavi <basavarajbhavi683@gmail.com>
2026-02-27 00:59:12 +0530

    Integrate Props component into app

--- React_basics/src/App.jsx
+++ React_basics/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "/src/components/Header/Header.jsx"
 import Footer from "/src/components/Footer/Footer.jsx"
 import  {Card}  from "/src/components/Card/Card.jsx"
+import Student from "./components/Props/Student.jsx"
 function App() {
   
 
@@ -10,6 +11,9 @@ function App() {
       <Footer />
       <Card />
       <Card />
+      <Student name="Basavaraj" age="21" isStudent={true}/>
+      <Student name="Virat" age="36" isStudent={false}/>
+
     </>
   )
 }
