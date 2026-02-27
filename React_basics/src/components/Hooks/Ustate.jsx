c6071bf3a2867bd0b0dab2e3de3fdcee439e8750
Basavaraj Bhavi <basavarajbhavi683@gmail.com>
2026-02-27 23:57:10 +0530

    Create Hook Folder and  Small count example

---
+++ React_basics/src/components/Hooks/Ustate.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+
+export const Ustate = () =>{
+    const [count,setCount] = useState(0);
+    
+    // Increment arrow function
+    const handlerIncrement= () =>{
+        setCount(count+1)
+    }
+
+    // Decrement arrow function
+    const handlerDecrement = () =>{
+        setCount(count-1)
+    }
+    // Reset arrow function
+    const handlerReset = () => {
+        setCount(0)
+    }
+    return(
+        <>
+            <div className="countContainer">
+                <h1>{count}</h1>
+                <button onClick={handlerIncrement}>Increment</button>
+                <button onClick={handlerDecrement}>Decrement</button>
+                <button onClick={handlerReset}>Reset</button>
+            </div>
+        </>
+        
+    );
+}
